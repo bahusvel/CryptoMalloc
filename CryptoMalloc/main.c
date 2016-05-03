db37c817dc3268752e3318b9a0dcaa047cec0617
Denis Lavrov <bahus.vel@gmail.com>
2016-05-04 01:30:53 +1200

    added calloc and realloc

--- CryptoMalloc/main.c
+++ CryptoMalloc/main.c
@@ -20,14 +20,14 @@ static void *(*libc_calloc)(size_t, size_t);
 static void (*libc_free)(void *);
 
 typedef struct MemNode{
-    int     fd;
-    size_t  alloc_size;
+    int				fd;
+	unsigned long	allocid;
+    size_t			alloc_size;
 } MemNode;
 
 typedef struct cor_map_node {
-    void *key;
-    int  fd;
-    size_t alloc_size;
+    void			*key;
+	MemNode			node;
     struct cor_map_node *next;
 } cor_map_node;
 
@@ -36,12 +36,11 @@ typedef struct cor_map{
     cor_map_node *last;
 } cor_map;
 
-int cor_map_get(cor_map* map, void *key, MemNode *node){
+static int cor_map_get(cor_map* map, void *key, MemNode *node){
     cor_map_node *np = map->first;
     while (np != NULL) {
         if (np->key == key) {
-            node->fd = np->fd;
-            node->alloc_size = np->alloc_size;
+			*node = np->node;
             return 1;
         }
         np = np->next;
@@ -49,24 +48,23 @@ int cor_map_get(cor_map* map, void *key, MemNode *node){
     return 0;
 }
 
-cor_map_node *create_node(void *key, int fd, size_t alloc_size){
-    cor_map_node *node = libc_malloc(sizeof(cor_map_node));
-    node->key = key;
-    node->fd = fd;
-    node->alloc_size = alloc_size;
-    node->next = NULL;
-    return node;
+static cor_map_node *create_node(void *key, MemNode *node){
+    cor_map_node *mnode = libc_malloc(sizeof(cor_map_node));
+    mnode->key = key;
+	mnode->node = *node;
+    mnode->next = NULL;
+    return mnode;
 }
 
-void cor_map_set(cor_map* map, void *key, int fd, size_t size){
-    MemNode node;
-    if (cor_map_get(map, key, &node) == 0) {
+static void cor_map_set(cor_map* map, void *key, MemNode *node){
+    MemNode tnode;
+    if (cor_map_get(map, key, &tnode) == 0) {
         if (map->first == NULL) {
-            map->first = create_node(key, fd, size);
+            map->first = create_node(key, node);
             map->last = map->first;
         } else {
             cor_map_node *last_node = map->last;
-            last_node->next = create_node(key, fd, size);
+            last_node->next = create_node(key, node);
             map->last = last_node->next;
         }
     }
@@ -76,24 +74,51 @@ void cor_map_set(cor_map* map, void *key, int fd, size_t size){
 static const char CRYPTO_PATH[] = "/Volumes/CryptoDisk/";
 static volatile unsigned long __crypto_allocid = 0;
 static cor_map mem_map;
-static const char *fend = "0";
+static const char fend = 0;
+
+__attribute__((constructor))
+static void crypto_malloc_ctor(){
+	if (!libc_malloc){
+		libc_malloc = dlsym(RTLD_NEXT, "malloc");
+		libc_calloc = dlsym(RTLD_NEXT, "realloc");
+		libc_realloc = dlsym(RTLD_NEXT, "calloc");
+		libc_free = dlsym(RTLD_NEXT, "free");
+		printf("It worked?");
+	}
+}
+
+__attribute__((destructor))
+static void crypto_malloc_dtor(){
+	cor_map_node *current = mem_map.first;
+	while (current != NULL) {
+		MemNode node = current->node;
+		munmap(current->key, node.alloc_size);
+		close(node.fd);
+		char path[sizeof(CRYPTO_PATH) + 20];
+		sprintf(path, "%s%016lx.mem", CRYPTO_PATH, node.allocid);
+		unlink(path);
+		current = current->next;
+	}
+}
 
 void* malloc(size_t size){
-    if (!libc_malloc){
-        libc_malloc = dlsym(RTLD_NEXT, "malloc");
-        libc_calloc = dlsym(RTLD_NEXT, "realloc");
-        libc_realloc = dlsym(RTLD_NEXT, "calloc");
-        libc_free = dlsym(RTLD_NEXT, "free");
-    }
+	if (!libc_malloc){
+		libc_malloc = dlsym(RTLD_NEXT, "malloc");
+		libc_calloc = dlsym(RTLD_NEXT, "realloc");
+		libc_realloc = dlsym(RTLD_NEXT, "calloc");
+		libc_free = dlsym(RTLD_NEXT, "free");
+	}
+							 
     char path[sizeof(CRYPTO_PATH) + 20];
     sprintf(path, "%s%016lx.mem", CRYPTO_PATH, __crypto_allocid++);
-    int fnum = open(path, O_RDWR | O_CREAT, 0);
+    int fnum = open(path, O_RDWR | O_CREAT, S_IRWXU);
     lseek(fnum, size - 1, SEEK_SET);
-    write(fnum, fend, 1);
+    write(fnum, &fend, 1);
     fsync(fnum);
     void *crypto_mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fnum, 0);
     if (crypto_mem != MAP_FAILED) {
-        cor_map_set(&mem_map, crypto_mem, fnum, size);
+		MemNode node = {fnum, __crypto_allocid - 1, size};
+        cor_map_set(&mem_map, crypto_mem, &node);
         return crypto_mem;
     } else {
         close(fnum);
@@ -104,9 +129,24 @@ void* malloc(size_t size){
 void free(void *ptr){
     //assert(mem_map != NULL);
     MemNode node;
-    cor_map_get(&mem_map, ptr, &node);
-    munmap(ptr, node.alloc_size);
-    close(node.fd);
+	if (cor_map_get(&mem_map, ptr, &node)){
+		munmap(ptr, node.alloc_size);
+		close(node.fd);
+		char path[sizeof(CRYPTO_PATH) + 20];
+		sprintf(path, "%s%016lx.mem", CRYPTO_PATH, node.allocid);
+		unlink(path);
+	} else {
+		libc_free(ptr);
+	}
+}
+
+// TODO: Use a more sophisticated realloc, for better performance
+void *realloc(void *ptr, size_t size){
+	free(ptr);
+	return malloc(size);
+}
+void *calloc(size_t count, size_t size){
+	return malloc(count * size);
 }
 
 /*
--- CryptoMallocTest/main.c
+++ CryptoMallocTest/main.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
