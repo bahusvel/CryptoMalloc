ca6dc6f254b40d6dffcc8c344bfad6208eae67e8
Denis Lavrov <bahus.vel@gmail.com>
2016-09-08 01:54:07 +1200

    Added demo files

---
+++ demo/README.md
@@ -0,0 +1,12 @@
+### Extract RW Memory
+```
+# sudo is neccessary to access other process pid
+sudo python3 pdump.py [pid]
+```
+### Regex stage
+```
+#Regex for email:
+grep -E -o '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b'
+#Regex for credit card number:
+grep -E -o '\b([0-9]{4}\-?){4}\b'
+```
---
+++ demo/pdump.py
@@ -0,0 +1,16 @@
+import sys
+
+
+maps = open("/proc/{}/maps".format(sys.argv[1]), mode='r')
+mem = open("/proc/{}/mem".format(sys.argv[1]), mode='rb')
+for line in maps.readlines():
+	parts = line.strip("\n").split(" ")
+	page_flags = parts[1]
+	if page_flags.startswith("rw"):
+		addr_range = parts[0].split("-")
+		mapping = parts[-1]
+		start_addr = int(addr_range[0], base=16)
+		end_addr = int(addr_range[1], base=16)
+		mem.seek(start_addr)
+		data = mem.read(end_addr-start_addr)
+		sys.stdout.buffer.write(data)
